4ef1b06f028aac58bc21e800032fee6803268f59
IsmaelSyed <26586274-IsmaelSyed@users.noreply.replit.com>
2023-09-21 20:20:00 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,63 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        string name, birthdate, workPhone, cellPhone;
+
+        // Unit Test #1
+        name = "Lucinda Potter";
+        birthdate = "6/24/1992";
+        workPhone = "000-000-0101";
+        cellPhone = "000-000-0189";
+
+        
+        Console.WriteLine(name);
+        Console.WriteLine(birthdate);
+        Console.WriteLine("work " + workPhone);
+        Console.WriteLine("cell " + cellPhone);
+        Console.WriteLine();
+
+        // Unit Test #2
+        name = "John Smith";
+        birthdate = "8/15/1978";
+        workPhone = "000-111-0101";
+        cellPhone = "000-111-0189";
+
+        
+        Console.WriteLine(name);
+        Console.WriteLine(birthdate);
+        Console.WriteLine("work " + workPhone);
+        Console.WriteLine("cell " + cellPhone);
+        Console.WriteLine();
+
+        // Unit Test #3
+        name = "Chris Johnson";
+        birthdate = "12/01/1987";
+        workPhone = "000-222-0101";
+        cellPhone = "000-222-0189";
+
+        
+        Console.WriteLine(name);
+        Console.WriteLine(birthdate);
+        Console.WriteLine("work " + workPhone);
+        Console.WriteLine("cell " + cellPhone);
+        Console.WriteLine();
+
+        // Unit Test #4
+        name = "Robert Hall";
+        birthdate = "2/27/1949";
+        workPhone = "000-444-0101";
+        cellPhone = "000-444-0189";
+
+        
+        Console.WriteLine(name);
+        Console.WriteLine(birthdate);
+        Console.WriteLine("work " + workPhone);
+        Console.WriteLine("cell " + cellPhone);
+
+        
+    }
+}
